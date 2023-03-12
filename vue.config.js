e48f635e100ebe88e77d755ccd0bf1227dfd120b
muxiaoqi-panrenjie <1748496336@qq.com>
2023-03-12 20:25:57 +0800

    提交测试

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
-const path = require('path');
+const path = require("path");
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: true
   //  配置方式一
   // outputDir:'./build',
   // configureWebpack: {
