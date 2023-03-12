10862d0a0ace4488023c948bfc8bf859971ac005
muxiaoqi-panrenjie <1748496336@qq.com>
2023-03-12 20:24:46 +0800

    测试

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
 const a = "22";
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require('path');
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //  配置方式一
+  // outputDir:'./build',
+  // configureWebpack: {
+  //   resolve: {
+  //     alias: {
+  //       components: '@/component',
+  //     }
+  //   }
+  // }
+  // 配置方式二
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@':path.resolve(__dirname,'src'),
+  //     components:'@/components'
+  //   }
+  // }
 });
